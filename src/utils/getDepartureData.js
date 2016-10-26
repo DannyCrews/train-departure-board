13793d6a18234ec8243a46b015b6b86c86db01cf
dannycrews <dancrews@me.com>
2016-10-26 12:21:57 -0400

    Catch error in axios promise

--- src/utils/getDepartureData.js
+++ src/utils/getDepartureData.js
@@ -4,6 +4,9 @@ function getDepartureData() {
   return axios.get('http://developer.mbta.com/lib/gtrtfs/Departures.csv')
   .then(function(result) {
     console.log(result)
+  })
+  .catch (function(error) {
+    console.log(error);
   });
 }
 
